451e49f6ea752758b7dd895b28918d31c4d9b551
Ditt Namn <linda-maria.m@hotmail.com>
2026-03-09 11:06:09 +0100

    Ändrat parameters namn i CategoryService och ICategoryService för ökad tydlighet.

--- Cyberquiz.BLL/Interfaces/ICategoryService.cs
+++ Cyberquiz.BLL/Interfaces/ICategoryService.cs
@@ -5,7 +5,7 @@ namespace Cyberquiz.BLL.Interfaces
     public interface ICategoryService
     {
         Task<IEnumerable<CategoryDto>> GetAllCategoriesAsync(string userName); 
-        Task<CategoryDto?> GetCategoryByIdAsync(string userName, int id);
+        Task<CategoryDto?> GetCategoryByIdAsync(string userName, int categoryId);
 
         Task<IEnumerable<SubCategoryDto>> GetAllSubCategoriesAsync(string userName);
         Task<SubCategoryDto?> GetSubCategoryByIdAsync(string userName, int id); 
